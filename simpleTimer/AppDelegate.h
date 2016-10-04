0026e88332c59194fb6be22fb1abe1a41c5deef9
chenxi_shan <924256668@qq.com>
2016-10-04 20:23:14 +0800

    Initial Commit

---
+++ simpleTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simpleTimer
+//
+//  Created by 单晨曦 on 2016/10/4.
+//  Copyright © 2016年 单晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ simpleTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simpleTimer
+//
+//  Created by 单晨曦 on 2016/10/4.
+//  Copyright © 2016年 单晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
