aba09d2d54da1956f168729afe32cc9e58490e6d
chenxi_shan <924256668@qq.com>
2016-10-04 23:11:56 +0800

    简单定时器

--- simpleTimer/ViewController.m
+++ simpleTimer/ViewController.m
@@ -9,6 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSTimer *timer;
+    BOOL isPlaying;
+    double num;
+    
+}
+@property (weak, nonatomic) IBOutlet UILabel *display;
+@property (weak, nonatomic) IBOutlet UIButton *playing;
+@property (weak, nonatomic) IBOutlet UIButton *pause;
+@property (weak, nonatomic) IBOutlet UIButton *reset;
+- (IBAction)playing:(UIButton *)sender;
+- (IBAction)pause:(UIButton *)sender;
+- (IBAction)reset:(UIButton *)sender;
 
 @end
 
@@ -16,7 +29,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+     num = 0.0;
+    
+    //double类型转成string类型
+    NSString *numString = [NSString stringWithFormat:@"%.1f", num];
+    
+    self.display.text = numString;
+    
 }
 
 
@@ -25,5 +45,45 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)timerFire:(id)userinfo {
+    
+    num = num + 0.1;
+    NSString *newNum = [NSString stringWithFormat:@"%.1f", num];
+    self.display.text = newNum;
+}
 
+
+- (IBAction)playing:(id)sender {
+    //创建定时器
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.1
+                                             target:self
+                                           selector:@selector(timerFire:)
+                                           userInfo:nil
+                                            repeats:YES];
+    [timer fire];
+    
+    _playing.enabled = NO;
+    _pause.enabled = YES;
+    isPlaying = YES;
+}
+
+- (IBAction)pause:(id)sender {
+
+    _playing.enabled = YES;
+    _pause.enabled = NO;
+    [timer invalidate];
+    timer = nil;
+    isPlaying = NO;
+}
+
+- (IBAction)reset:(id)sender {
+
+    [timer invalidate];
+    isPlaying = NO;
+    num = 0.0;
+    NSString *setNum = [NSString stringWithFormat:@"%.1f", num];
+    self.display.text = setNum;
+    _playing.enabled = YES;
+    _pause.enabled = YES;
+}
 @end
